c8796f1addc9684384bfd55ffeae2363297ced7e
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2023-10-02 15:41:03 +0200

    gg

--- fun.js
+++ fun.js
@@ -1,8 +1,9 @@
 "use strict";
 
-function Student(firstname, lastname) {
+function Student(firstname, lastname, height) {
   this.firstName = firstname;
   this.lastName = lastname;
+  this.height = height;
   Object.defineProperty(this, "fullName", {
     get: function () {
       return this.firstName + " " + this.lastName;
@@ -11,13 +12,32 @@ function Student(firstname, lastname) {
   this.toString = function () {
     return this.fullName;
   };
+  this.valueOf = function () {
+    return this.height;
+  };
 }
 
-const harry = new Student("Harry", "Potter");
-const ron = new Student("Ron", "Weasley");
+const harry = new Student("Harry", "Potter", 170);
+const ron = new Student("Ron", "Weasley", 173);
+const hermione = new Student("Hermione", "Granger", 176);
 Object.defineProperty(ron, "pet", { value: "Rat", enumerable: true, configurable: true, writable: true });
 
 console.log(harry);
 console.log("Something about " + harry.toString());
 console.log("lfmdlsm " + harry.fullName);
 console.log("hihi " + harry);
+
+const students = [harry, ron, hermione];
+
+// console.log(students);
+
+students.sort();
+// students.sort().reverse();
+
+console.log(students);
+
+console.log("Harry er: " + harry);
+// output = Harry er: 170
+
+console.log(`Harry er: ${harry}`);
+//  output = Harry er: Harry Potter
